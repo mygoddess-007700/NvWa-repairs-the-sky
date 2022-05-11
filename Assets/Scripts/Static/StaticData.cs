8e2159111f6012606cc9a3a42584ea0a9fa34647
fxd <mygoddess007700@gmail.com>
2022-05-11 21:16:36 +0800

    :sparkles: feat: 数据存储

---
+++ Assets/Scripts/Static/StaticData.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StaticData : MonoBehaviour
+{
+    // public static int correct;
+    // public static int error;
+    public static int score;
+}
